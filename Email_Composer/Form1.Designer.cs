b20a650f024d35d6b664ba210ecaa02c5ee23bec
AladdinKamal <aladdin.kamal7@gmail.com>
2016-02-28 03:04:10 +0200

    Update v1.2
    -Bug fixes.
    -Added some features.

--- Email_Composer/Form1.Designer.cs
+++ Email_Composer/Form1.Designer.cs
@@ -44,15 +44,21 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Name = "label1";
             // 
             // label2
@@ -110,9 +116,12 @@
             // 
             // textBox1
             // 
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
             resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
             this.toolTip1.SetToolTip(this.textBox1, resources.GetString("textBox1.ToolTip"));
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // textBox2
             // 
@@ -128,36 +137,68 @@
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem,
+            this.modeToolStripMenuItem,
+            this.aboutToolStripMenuItem,
             this.exitToolStripMenuItem1});
             resources.ApplyResources(this.menuStrip1, "menuStrip1");
             this.menuStrip1.Name = "menuStrip1";
             // 
-            // exitToolStripMenuItem
+            // modeToolStripMenuItem
             // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lightModeToolStripMenuItem,
+            this.darkModeToolStripMenuItem});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            resources.ApplyResources(this.modeToolStripMenuItem, "modeToolStripMenuItem");
+            // 
+            // lightModeToolStripMenuItem
+            // 
+            this.lightModeToolStripMenuItem.Checked = true;
+            this.lightModeToolStripMenuItem.CheckOnClick = true;
+            this.lightModeToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.lightModeToolStripMenuItem.Name = "lightModeToolStripMenuItem";
+            resources.ApplyResources(this.lightModeToolStripMenuItem, "lightModeToolStripMenuItem");
+            this.lightModeToolStripMenuItem.Click += new System.EventHandler(this.lightModeToolStripMenuItem_Click);
+            // 
+            // darkModeToolStripMenuItem
+            // 
+            this.darkModeToolStripMenuItem.CheckOnClick = true;
+            this.darkModeToolStripMenuItem.Name = "darkModeToolStripMenuItem";
+            resources.ApplyResources(this.darkModeToolStripMenuItem, "darkModeToolStripMenuItem");
+            this.darkModeToolStripMenuItem.Click += new System.EventHandler(this.darkModeToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem1
             // 
+            this.exitToolStripMenuItem1.BackColor = System.Drawing.SystemColors.Control;
             this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
             resources.ApplyResources(this.exitToolStripMenuItem1, "exitToolStripMenuItem1");
             this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.groupBox1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
@@ -166,8 +207,9 @@
             this.groupBox1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -187,8 +229,12 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkModeToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Email_Composer/Form1.cs
+++ Email_Composer/Form1.cs
@@ -18,11 +18,12 @@ namespace Email_Composer
         public Form1()
         {
             InitializeComponent();
+            textBox2.Select();
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("\t\tEmail Composer v1.0\n\n This program current supports google and yahoo accounts only.");
+            MessageBox.Show("\t\tEmail Composer v1.2\n\n This program currently supports google and yahoo accounts only.");
         }
 
         private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
@@ -32,9 +33,16 @@ namespace Email_Composer
 
         private void textBox4_Enter(object sender, EventArgs e)
         {
-            if (textBox4.Text == "Account@Example.com")
+            if (textBox4.Text.Contains("@Example.com"))
                 textBox4.Clear();
-            textBox4.ForeColor = Color.Black;
+            if (darkModeToolStripMenuItem.Selected == true)
+            {
+                textBox4.ForeColor = Color.White;
+            }
+            else
+            {
+                textBox4.ForeColor = Color.Black;
+            }
         }
 
         private void textBox4_Leave(object sender, EventArgs e)
@@ -50,7 +58,14 @@ namespace Email_Composer
         {
             if (textBox5.Text == "XxxxxxxX")
                 textBox5.Clear();
-            textBox5.ForeColor = Color.Black;
+            if (darkModeToolStripMenuItem.Selected == true)
+            {
+                textBox5.ForeColor = Color.White;
+            }
+            else
+            {
+                textBox5.ForeColor = Color.Black;
+            }
         }
 
         private void textBox5_Leave(object sender, EventArgs e)
@@ -59,6 +74,28 @@ namespace Email_Composer
             {
                 textBox5.ForeColor = Color.Gray;
                 textBox5.Text = "XxxxxxxX";
+            }
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("@Example.com"))
+                textBox1.Clear();
+            if (darkModeToolStripMenuItem.Selected == true)
+            {
+                textBox1.ForeColor = Color.White;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.ForeColor = Color.Gray;
+                textBox1.Text = "Account1@Example.com ; Account2@Example.com";
             }
         }
 
@@ -81,7 +118,7 @@ namespace Email_Composer
                     message.From = new MailAddress(textBox4.Text);
                     message.Subject = textBox2.Text;
                     message.Body = textBox3.Text;
-                    foreach (string s in textBox1.Text.Split(';'))
+                    foreach (string s in textBox1.Text.Trim().Split(';'))
                     {
                         message.To.Add(s);
                     }
@@ -118,6 +155,106 @@ namespace Email_Composer
                     button1.Enabled = true;
                 }
             }
+        }
+
+        private void lightModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            darkModeToolStripMenuItem.Checked = false;
+            label1.ForeColor = Color.Black;
+            label2.ForeColor = Color.Black;
+            label3.ForeColor = Color.Black;
+            label4.ForeColor = Color.Black;
+            label5.ForeColor = Color.Black;
+            label1.BackColor = SystemColors.Control;
+            label2.BackColor = SystemColors.Control;
+            label3.BackColor = SystemColors.Control;
+            label4.BackColor = SystemColors.Control;
+            label5.BackColor = SystemColors.Control;
+            button1.ForeColor = Color.Black;
+            button1.BackColor = SystemColors.Control;
+            if (textBox1.Text.Contains("@Example.com"))
+                textBox1.ForeColor = Color.Gray;
+            else
+                textBox1.ForeColor = Color.Black;
+            textBox2.ForeColor = Color.Black;
+            textBox3.ForeColor = Color.Black;
+            if (textBox4.Text.Contains("@Example.com"))
+                textBox4.ForeColor = Color.Gray;
+            else
+                textBox4.ForeColor = Color.Black;
+            if (textBox5.Text == "XxxxxxxX")
+                textBox5.ForeColor = Color.Gray;
+            else
+                textBox5.ForeColor = Color.Black;
+            textBox1.BackColor = SystemColors.Window;
+            textBox2.BackColor = SystemColors.Window;
+            textBox3.BackColor = SystemColors.Window;
+            textBox4.BackColor = SystemColors.Window;
+            textBox5.BackColor = SystemColors.Window;
+            modeToolStripMenuItem.ForeColor = Color.Black;
+            modeToolStripMenuItem.BackColor = SystemColors.Control;
+            lightModeToolStripMenuItem.ForeColor = Color.Black;
+            darkModeToolStripMenuItem.ForeColor = Color.Black;
+            lightModeToolStripMenuItem.BackColor = SystemColors.Control;
+            darkModeToolStripMenuItem.BackColor = SystemColors.Control;
+            aboutToolStripMenuItem.ForeColor = Color.Black;
+            aboutToolStripMenuItem.BackColor = SystemColors.Control;
+            exitToolStripMenuItem1.ForeColor = Color.Black;
+            exitToolStripMenuItem1.BackColor = SystemColors.Control;
+            groupBox1.ForeColor = Color.Black;
+            groupBox1.BackColor = SystemColors.Control;
+            panel1.BackColor = SystemColors.Control;
+            menuStrip1.BackColor = SystemColors.Control;
+        }
+
+        private void darkModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lightModeToolStripMenuItem.Checked = false;
+            label1.ForeColor = Color.White;
+            label2.ForeColor = Color.White;
+            label3.ForeColor = Color.White;
+            label4.ForeColor = Color.White;
+            label5.ForeColor = Color.White;
+            label1.BackColor = Color.Black;
+            label2.BackColor = Color.Black;
+            label3.BackColor = Color.Black;
+            label4.BackColor = Color.Black;
+            label5.BackColor = Color.Black;
+            button1.ForeColor = Color.White;
+            button1.BackColor = Color.Black;
+            if (textBox1.Text.Contains("@Example.com"))
+                textBox1.ForeColor = Color.Gray;
+            else
+                textBox1.ForeColor = Color.White;
+            textBox2.ForeColor = Color.White;
+            textBox3.ForeColor = Color.White;
+            if (textBox4.Text.Contains("@Example.com"))
+                textBox4.ForeColor = Color.Gray;
+            else
+                textBox4.ForeColor = Color.White;
+            if (textBox5.Text == "XxxxxxxX")
+                textBox5.ForeColor = Color.Gray;
+            else
+                textBox5.ForeColor = Color.White;
+            textBox1.BackColor = Color.Black;
+            textBox2.BackColor = Color.Black;
+            textBox3.BackColor = Color.Black;
+            textBox4.BackColor = Color.Black;
+            textBox5.BackColor = Color.Black;
+            modeToolStripMenuItem.ForeColor = Color.White;
+            modeToolStripMenuItem.BackColor = Color.Black;
+            lightModeToolStripMenuItem.ForeColor = Color.White;
+            darkModeToolStripMenuItem.ForeColor = Color.White;
+            lightModeToolStripMenuItem.BackColor = Color.Black;
+            darkModeToolStripMenuItem.BackColor = Color.Black;
+            aboutToolStripMenuItem.ForeColor = Color.White;
+            aboutToolStripMenuItem.BackColor = Color.Black;
+            exitToolStripMenuItem1.ForeColor = Color.White;
+            exitToolStripMenuItem1.BackColor = Color.Black;
+            groupBox1.ForeColor = Color.White;
+            groupBox1.BackColor = Color.Black;
+            panel1.BackColor = Color.Black;
+            menuStrip1.BackColor = Color.Black;
         }
     }
 }
